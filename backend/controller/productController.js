5fc68bffc31e58ba7642082575255832b115e19f
Rakeem Shaikh <rakeemshaikh4545@gmail.com>
2024-03-05 23:55:08 +0530

    implementing redux

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -17,7 +17,7 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 //Get all products
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-  const resultPerPage = 5;
+  const resultPerPage = 8;
   const productCount = await Product.countDocuments();
 
   const apiFeatures = new ApiFeatures(Product.find(), req.query)
